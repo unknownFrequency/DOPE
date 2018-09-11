2b03c8fe25e501413d6a740e4b26ac356730bbd6
Your Name <unknownfreq@protonmail.com>
2018-09-11 14:36:50 +0200

    refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,3 @@
-require 'net/http'
-
 class UsersController < ApplicationController
 
   def login
@@ -10,15 +8,16 @@ class UsersController < ApplicationController
     base64_encoded_client_id_and_secret = base64_encode params[:user][:client_id], params[:user][:client_secret]
 
     respond_to do |format|
-      @user = User.new user_params unless User.exists? email: params[:user][:email] 
+      email = params[:user][:email]
+      @user = User.new user_params unless User.exists? email: email 
 
       if @user && @user.save 
 	create_session( base64_encoded_client_id_and_secret, @user.api_key)
-	flash[:notice] = "Du er nu oprettet i databasen med email: #{params[:user][:email]}"
+	flash[:notice] = "Du er nu oprettet i databasen med email: #{email}"
 	format.html { redirect_to user_path @user }
 	format.json { render :show }
       elsif User.exists? email: params[:user][:email]
-	@user = User.where(email: params[:user][:email]).take
+	@user = User.where(email: email).take
 	create_session(base64_encoded_client_id_and_secret, @user.api_key) 
 	format.html { redirect_to user_path @user }
 	format.json { render :show }
